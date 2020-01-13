e56a1b6dc29fd2b07b93e84fe52c60c854ef58cd
Boomkop3 <sjors.de.bruijn@gmail.com>
2020-01-13 17:44:07 +0100

    Le map, radiobuttons, and settings button now twerk

--- app/app/src/main/java/com/example/stormchaserapp/MapsActivity.java
+++ app/app/src/main/java/com/example/stormchaserapp/MapsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,7 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
 
     public void startSettings() {
         Intent intent = new Intent(this, SettingsFragment.class);
@@ -28,7 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        Button settings = findViewById(R.id.buttonSettings);
+        ImageButton settings = findViewById(R.id.buttonSettings);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,11 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map = googleMap;
+        
     }
 }
