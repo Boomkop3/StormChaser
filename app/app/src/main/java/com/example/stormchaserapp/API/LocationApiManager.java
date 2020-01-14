5ad41b0944814a1a98af168d8eb93ed64ee9d370
Boomkop3 <sjors.de.bruijn@gmail.com>
2020-01-14 02:48:45 +0100

    Refactored location api

---
+++ app/app/src/main/java/com/example/stormchaserapp/API/LocationApiManager.java
@@ -0,0 +1,101 @@
+package com.example.stormchaserapp.API;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.stormchaserapp.lib.Toaster;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class LocationApiManager {
+    private static LocationApiManager instance;
+    private Context context;
+    private FusedLocationProviderClient fusedLocationClient;
+    public static LocationApiManager with(Context context){
+        if (instance == null){
+            instance = new LocationApiManager(context);
+        }
+        return instance;
+    }
+    private LocationApiManager(Context context){
+        this.context = context;
+    }
+    public void getFusedLocation(OnSuccessListener onSuccessListener){
+        if (checkLocationPermission()) {
+            if (fusedLocationClient == null){
+                fusedLocationClient =
+                        LocationServices.getFusedLocationProviderClient(
+                                context
+                        );
+            }
+            fusedLocationClient
+                    .getLastLocation()
+                    .addOnSuccessListener(onSuccessListener);
+        }
+        else {
+            throw new IllegalStateException("Permission to get user location denied");
+        }
+    }
+    public void getLocationPermission(Activity activity){
+        if (!checkLocationPermission()){
+            ActivityCompat.requestPermissions(
+                    activity,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    // Google's documentation is kinda bad
+                    (int)(Math.random() * 1337) // f this
+            );
+        }
+    }
+    public boolean checkLocationPermission(){
+        return ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void startListeningUserLocation(LocationListener locationListener) {
+        Criteria locationCriteria = new Criteria();
+        locationCriteria.setAccuracy(Criteria.ACCURACY_FINE);
+        locationCriteria.setAltitudeRequired(false);
+        locationCriteria.setBearingRequired(false);
+        locationCriteria.setSpeedRequired(false);
+        locationCriteria.setCostAllowed(true);
+        LocationManager locationManager =
+                (LocationManager) context.getSystemService(
+                        context.LOCATION_SERVICE
+                );
+        if (checkLocationPermission()) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    1,
+                    locationListener,
+                    null
+            );
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        else {
+            throw new IllegalStateException("Permission to get user location denied");
+        }
+    }
+}
--- app/app/src/main/java/com/example/stormchaserapp/MapsActivity.java
+++ app/app/src/main/java/com/example/stormchaserapp/MapsActivity.java
@@ -2,22 +2,32 @@ package com.example.stormchaserapp;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.stormchaserapp.API.LocationApiManager;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity
+        extends     FragmentActivity
+        implements  OnMapReadyCallback,
+                    LocationListener,
+                    OnSuccessListener<Location> {
     private GoogleMap map;
+    private LocationApiManager locationApiManager;
+    private Context context;
 
     public void startSettings() {
         Intent intent = new Intent(this, SettingsFragment.class);
@@ -27,8 +37,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.context = getApplicationContext();
+        this.locationApiManager = LocationApiManager.with(context);
         setContentView(R.layout.activity_maps);
-
         ImageButton settings = findViewById(R.id.buttonSettings);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,25 +47,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startSettings();
             }
         });
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        
+        // Start the map using lastKnownLocation from fusedLocation
+        // Then switch to gps as soon as it's available
+        if (locationApiManager.checkLocationPermission()){
+            locationApiManager.getFusedLocation(this);
+            locationApiManager.startListeningUserLocation(this);
+        }
+        else {
+            locationApiManager.getLocationPermission(this);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == requestCode) {
+            if (locationApiManager.checkLocationPermission()) {
+                locationApiManager.startListeningUserLocation(this);
+            } else {
+                // Otherwise, just ask again
+                locationApiManager.getLocationPermission(this);
+            }
+        }
+    }
+
+    private void handleLocationUpdate(Location location){
+        LatLng locationPoint = new LatLng(
+                location.getLatitude(),
+                location.getLongitude()
+        );
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(locationPoint);
+        circleOptions.clickable(false);
+        circleOptions.radius(1000);
+        circleOptions.fillColor(0);
+        map.addCircle(circleOptions);
+        map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                        locationPoint,
+                        10
+                )
+        );
+    }
+
+    // fusedLocation
+    @Override
+    public void onSuccess(Location location) {
+        if (location != null){
+            handleLocationUpdate(location);
+            return;
+        }
+        new AlertDialog.Builder(this)
+                .setTitle(context.getResources().getString(
+                        R.string.location_required_dialog_title))
+                .setMessage(context.getResources().getString(
+                        R.string.location_required_dialog_message))
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleLocationUpdate(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        // wtvr
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        // wtvr
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        // wtvr
+    }
+}
+
+interface onCoordinateResponse {
+    void onResponse(LatLng location);
 }
