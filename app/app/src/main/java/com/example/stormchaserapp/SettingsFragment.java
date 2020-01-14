125a4db89f5e75a576e953b124d0650e93198efd
Tom <tomkaas74@gmail.com>
2020-01-14 15:48:53 +0100

    Language now immediately switches

--- app/app/src/main/java/com/example/stormchaserapp/SettingsFragment.java
+++ app/app/src/main/java/com/example/stormchaserapp/SettingsFragment.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +22,35 @@ import android.widget.RadioButton;
 import com.example.stormchaserapp.Storage.DataManager;
 import com.example.stormchaserapp.Storage.Language;
 
+import java.util.Locale;
+
 public class SettingsFragment extends AppCompatActivity {
 
     private RadioButton NL;
     private RadioButton EN;
+
+
+    private void setLocale(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        Resources resources = getBaseContext().getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = new Locale(lang);
+        getBaseContext().getApplicationContext().createConfigurationContext(configuration);
+
+        Configuration cfg = new Configuration();
+        if (!TextUtils.isEmpty(lang)) {
+            cfg.locale = new Locale(lang);
+        }
+        else {
+            cfg.locale = Locale.getDefault();
+        }
+        this.getResources().updateConfiguration(cfg, null);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -50,12 +78,16 @@ public class SettingsFragment extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 DataManager.with(getApplicationContext()).setLanguage(Language.nl);
+                setLocale(Language.nl.name());
+                recreate();
             }
         });
         EN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DataManager.with(getApplicationContext()).setLanguage(Language.en);
+                setLocale(Language.en.name());
+                recreate();
             }
         });
     }
